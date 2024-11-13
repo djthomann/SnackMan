5f03406cff0ec49c776783fae1f6bce99331bd2f
David Thomann <david.thomann@student.hs-rm.de>
2024-11-13 13:17:18 +0100

    FEAT Spring Initialization

---
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/SnackmanApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swt.projekt.snackman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsrm/mi/swt/projekt/snackman/SnackmanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swt.projekt.snackman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
