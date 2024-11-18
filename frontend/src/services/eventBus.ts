9eca25bfbda6b226bafb3be711d082f53e7cf121
David Thomann <david.thomann@student.hs-rm.de>
2024-11-18 21:06:16 +0100

    Added SocketService usage to Scene (WASD) and eventBus explanation

--- frontend/src/services/eventBus.ts
+++ frontend/src/services/eventBus.ts
@@ -1,7 +1,8 @@
 import mitt from 'mitt';
 
 /**
- * Service that 
+ * Service that can be subribed to, emits an event to listeners
+ * Used by socketService to inform subscribers that a server message has arrived
  */
 
 const eventBus = mitt<{ serverMessage: string }>();
