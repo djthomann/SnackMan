fd1fae53fcd3c98b0184545d96dfc72f5f2cfb5a
David Thomann <david.thomann@student.hs-rm.de>
2024-11-20 16:04:16 +0100

    FEAT/#20: Added FetchMap Vue-Component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Scene from './components/Scene.vue'
 import Username from './components/Username.vue'
+import FetchMap from './components/FetchMap.vue';
 </script>
 
 <template>
+  <FetchMap />
   <Username />
   <Scene />
 </template>
---
+++ frontend/src/components/FetchMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="loadMap">Map laden</button>
+</template>
+
+<script setup lang="ts">
+import eventBus from '@/services/eventBus'
+import useWebSocket from '@/services/socketService'
+
+const { serverResponse, connect, sendMessage, closeConnection } = useWebSocket()
+
+const loadMap = () => {
+  console.log('loading map')
+  sendMessage('Das ist meine Anfrage')
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+button {
+  background-color: black;
+  color: white;
+  border: none;
+  float: right;
+  margin-top: 20px;
+  margin-right: 20px;
+  font-size: 14pt;
+}
+</style>
