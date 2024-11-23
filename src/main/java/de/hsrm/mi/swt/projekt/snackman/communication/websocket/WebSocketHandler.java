058bbc1e1ea50ef71606a3cd5b723b79ad53b92b
David Thomann <david.thomann@student.hs-rm.de>
2024-11-23 01:05:46 +0100

    FEAT/#20: Tested Loading with larger, generated Map

--- src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/websocket/WebSocketHandler.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/websocket/WebSocketHandler.java
@@ -61,7 +61,9 @@ public class WebSocketHandler extends TextWebSocketHandler {
             sendClientInfo();
         } else if(messageString.startsWith("MAP")) {
 
-            SnackManMap map = new SnackManMap(MapGenerationConfig.SAVED_MAPS_PATH + "testFile.csv");
+            SnackManMap map = new SnackManMap(MapGenerationConfig.SAVED_MAPS_PATH + "map_2024-11-23_01:02:48.csv");
+            // SnackManMap map = new SnackManMap(40, 40);
+            map.saveAsCSV();
 
             logger.info("Phony Data:" + map.toString());
 
