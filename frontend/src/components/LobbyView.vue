bd66253dc777308a55778f353b1a982efcffc268
Atta Farsimadan <atta.farsimadan@student.hs-rm.de>
2024-11-25 13:19:31 +0100

    FEAT/#69: implemented HomeView and additional routes

---
+++ frontend/src/components/LobbyView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Lobby {{ lobbyCode }}</h1>
+  <button @click="startGame">Start Game</button>
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const router = useRouter();
+const lobbyCode = computed(() => route.params.code);
+
+const startGame = () => {
+  router.push('/game/' + lobbyCode.value);
+};
+</script>
+
+<style scoped></style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,10 +2,16 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/components/HomeView.vue';
 import TitleView from '@/components/TitleView.vue';
+import LobbyView from '@/components/LobbyView.vue';
+import Username from '@/components/Username.vue';
+import Scene from '@/components/Scene.vue';
 
 const routes = [
   { path: '/', component: TitleView },
   { path: '/home', component: HomeView },
+  { path: '/lobby/:code', component: LobbyView },
+  { path: '/username', component: Username },
+  { path: '/game/:code', component: Scene },
 ];
 
 const router = createRouter({
