7d1576775acf0dc1f76c9727911fed21cb2fe799
David Thomann <david.thomann@student.hs-rm.de>
2024-11-27 15:05:24 +0100

    FEAT/#116: Removed Usage of old IDFactory

--- src/main/java/de/hsrm/mi/swt/projekt/snackman/logic/Lobby.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/logic/Lobby.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.hsrm.mi.swt.projekt.snackman.configuration.GameConfig;
-import de.hsrm.mi.swt.projekt.snackman.model.gameEntities.IDFactory;
+import de.hsrm.mi.swt.projekt.snackman.model.gameEntities.IDGenerator;
 import de.hsrm.mi.swt.projekt.snackman.model.level.SnackManMap;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,8 +14,10 @@ public class Lobby {
     
     Logger logger = LoggerFactory.getLogger(Lobby.class);
 
-    IDFactory idFactory = IDFactory.getInstance();
-    private long id = idFactory.getUniqueID();
+    private IDGenerator idGenerator = IDGenerator.getInstance();
+
+    /** The unique identifier for the Food */
+    private long id = idGenerator.getUniqueID();
 
     private GameConfig gameConfig;
     private SnackManMap map;
