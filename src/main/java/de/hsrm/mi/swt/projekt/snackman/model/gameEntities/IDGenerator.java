1f3ef1deaacf5c6cf90a9231ff896176d934433f
David Thomann <david.thomann@student.hs-rm.de>
2024-11-27 14:53:28 +0100

    BUGFIX/#145: Added new way of generating IDs

---
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/model/gameEntities/IDGenerator.java
@@ -0,0 +1,27 @@
+package de.hsrm.mi.swt.projekt.snackman.model.gameEntities;
+
+public class IDGenerator {
+
+    private static IDGenerator instance;
+
+    private long counter;
+
+    private IDGenerator() {
+        counter = 1;
+    }
+
+    public static IDGenerator getInstance() {
+        if(instance == null) {
+            instance = new IDGenerator();
+            return instance;
+        } else {
+            return instance;
+        }
+    }
+
+    synchronized public long getUniqueID() {
+        counter++;
+        return counter;
+    }
+
+}
