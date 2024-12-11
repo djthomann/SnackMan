1138b23cee02eba2fe162d799b5b47fee4a5aadf
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2024-12-11 14:28:22 +0100

    FEAT/#240: added id to constructor of ghost and snackman

--- src/main/java/de/hsrm/mi/swt/projekt/snackman/model/gameEntities/GameObject.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/model/gameEntities/GameObject.java
@@ -3,21 +3,25 @@ package de.hsrm.mi.swt.projekt.snackman.model.gameEntities;
 public class GameObject {
 
     protected final long gameId;
-    protected final long id;
+    protected final long objectId;
     protected float x;
     protected float y;
     protected float z;
 
     public GameObject(long gameId, float x, float y, float z) {
+        this(IDGenerator.getInstance().getUniqueID(), gameId, x, y, z);
+    }
+
+    public GameObject(long objectId, long gameId, float x, float y, float z) {
+        this.objectId = objectId;
         this.gameId = gameId;
         this.x = x;
         this.y = y;
         this.z = z;
-        this.id = IDGenerator.getInstance().getUniqueID();
     }
 
-    public long getId() {
-        return id;
+    public long getObjectId() {
+        return objectId;
     }
 
     public float getX() {
