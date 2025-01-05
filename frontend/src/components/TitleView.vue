7ec508b9ab1bfc589003d1afaf782279cfd21139
(Sina) Larissa Oblong <sinalarissa.oblong@student.hs-rm.de>
2025-01-05 21:56:46 +0100

    FEAT/#216: base css for title view

--- frontend/src/components/TitleView.vue
+++ frontend/src/components/TitleView.vue
@@ -1,8 +1,12 @@
 <template>
-  <h1>Title</h1>
-  <p>Please enter your username</p>
-  <input type="text" v-model="usernameInputField" />
-  <button @click="submitForm">Submit</button>
+    <div class="titleview">
+      <div class="titleview__titleimage"></div>
+      <div class="titleview__panel">
+        <label for="usernameInputField">Player-Username:</label>
+        <input type="text" v-model="usernameInputField" class="usernameInputField"/>
+        <button @click="submitForm">Submit</button>
+      </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -11,11 +15,11 @@ import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/userStore';
 import useWebSocket from '@/services/socketService';
 
-const usernameInputField = ref('');
 const router = useRouter();
 const { sendMessage, onMessage } = useWebSocket();
 const serverMessage = ref<string>('');
 const userStore = useUserStore();
+const usernameInputField = ref('');
 
 const submitForm = () => {
   const message = JSON.stringify({ type: 'REGISTERUSERNAME', username: usernameInputField.value });
@@ -33,9 +37,61 @@ const handleServerMessage = (message: string) => {
     router.push({ path: '/home'});
   }
 }
-
 onMessage(handleServerMessage);
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.titleview {
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  background-image: url('../assets/images/backgrounds/backgroundcontainer_image.jpg');
+  background-color: var(--colorSecondary);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.titleview__titleimage {
+  width: 50dvw;
+  height: 50dvh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-image: url('../assets/images/decorations/skateboard.png');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.titleview__panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url('../assets/images/backgrounds/titleview_backgroundpanel.png');
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 5% 10% 5% 10%;
+}
+
+label {
+  align-self: flex-start;
+  padding: 0 5% 5% 0;
+}
+
+input {
+  border: none;
+  width: 100%;
+  border-radius: 5px;
+}
+
+
+</style>
