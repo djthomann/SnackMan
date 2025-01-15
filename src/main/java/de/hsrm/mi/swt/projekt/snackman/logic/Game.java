de97208958c4e3dca5376a30af870c20e0f3058c
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2025-01-15 16:14:50 +0100

    FEAT/#330: fix snackman spawn height

--- src/main/java/de/hsrm/mi/swt/projekt/snackman/logic/Game.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/logic/Game.java
@@ -98,7 +98,7 @@ public class Game {
      * @param id said id
      */
     private void spawnGhost(long id, String username) {
-        Ghost ghost = new Ghost(username, id, this.id, (float) map.getW() / 2.0f, 0.5f, (float) map.getH() / 2.0f, this.gameConfig, this.gameManager, this.collisionManager);
+        Ghost ghost = new Ghost(username, id, this.id, (float) map.getW() / 2.0f, 0, (float) map.getH() / 2.0f, this.gameConfig, this.gameManager, this.collisionManager);
         allMovables.add(ghost);
         map.getTileAt((int) ghost.getX(), (int) ghost.getZ()).addToOccupation(ghost);
     }
@@ -132,7 +132,7 @@ public class Game {
         x += 0.5f;
         z += 0.5f;
 
-        SnackMan snackMan = new SnackMan(username, id, this.id, x, 0.5f, z, this.gameManager, this.gameConfig, this.collisionManager); 
+        SnackMan snackMan = new SnackMan(username, id, this.id, x, 0, z, this.gameManager, this.gameConfig, this.collisionManager);
         allMovables.add(snackMan);
         map.getTileAt((int) x, (int) z).addToOccupation(snackMan);
         numSnackmen++;
