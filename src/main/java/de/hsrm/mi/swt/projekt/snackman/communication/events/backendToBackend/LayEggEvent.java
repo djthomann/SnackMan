4b332e653b25535a080f5cd0ef4a91834ac617fc
Laura Wiatrek <laura.wiatrek@student.hs-rm.de>
2025-01-15 13:13:22 +0100

    FEAT/#245: eggs get created in FE (as broccoli right now)

--- src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/events/backendToBackend/LayEggEvent.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/events/backendToBackend/LayEggEvent.java
@@ -2,6 +2,7 @@ package de.hsrm.mi.swt.projekt.snackman.communication.events.backendToBackend;
 
 import org.springframework.context.ApplicationEvent;
 
+import de.hsrm.mi.swt.projekt.snackman.logic.GameManager;
 import de.hsrm.mi.swt.projekt.snackman.model.gameEntities.Food;
 
 public class LayEggEvent extends ApplicationEvent {
@@ -9,6 +10,7 @@ public class LayEggEvent extends ApplicationEvent {
 
     private Food food;
     private long gameId;
+    private final GameManager gameManager;
 
     /**
      * Event that is called upon when a chicken lays an egg.
@@ -16,10 +18,11 @@ public class LayEggEvent extends ApplicationEvent {
      * @param food the food that was eaten.
      * @param id id of the game the event belongs to.
      */
-    public LayEggEvent(Object source, Food food, long id) {
+    public LayEggEvent(Object source, Food food, long id, GameManager gameManager) {
         super(source);
         this.food = food;
         this.gameId = id;
+        this.gameManager = gameManager;
     }
 
     public Food getFood() {
@@ -28,6 +31,10 @@ public class LayEggEvent extends ApplicationEvent {
 
     public long getGameId() {
         return gameId;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
     }
 
 }
--- src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/events/backendToBackend/LayEggEventListener.java
+++ src/main/java/de/hsrm/mi/swt/projekt/snackman/communication/events/backendToBackend/LayEggEventListener.java
@@ -4,20 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 import de.hsrm.mi.swt.projekt.snackman.logic.GameManager;
 import de.hsrm.mi.swt.projekt.snackman.logic.GameState;
 
+@Component
 public class LayEggEventListener implements ApplicationListener <LayEggEvent> {
 
     Logger logger = LoggerFactory.getLogger(LayEggEventListener.class);
 
-    @Autowired
-    GameManager gameManager;
-
     @Override
     public void onApplicationEvent(LayEggEvent event) {
-        GameState gameState = gameManager.getGameById(event.getGameId()).getGameState();
+        GameState gameState = event.getGameManager().getGameById(event.getGameId()).getGameState();
         gameState.addLaidEgg(event.getFood());
+        logger.info("LayEggListener aufgerufen");
     }
 }
